0e61dd9e7b8e3c7ed44e6c729fd132b598eb3a39
dacappo <patrickspiegel@web.de>
2014-11-10 15:13:20 +0100

    Server is up and running with mysql package

--- Server/main.js
+++ Server/main.js
@@ -2,13 +2,30 @@
 	"use strict";
 
 	var http = require("http");
-	var url = require("url");
 	http.createServer(function (request, response) {
 		// Get GET parameters from URL
+		var url = require("url");
 		var parameters = url.parse(request.url, true).query;
+
+		// Connect to MySql database
+		var mysql = require("mysql");
+		var connection = mysql.createConnection({
+			host : "localhost",
+			user : "",
+			password : ""
+		});
+
+		connection.connect();
+
+		connection.query("INSERT INTO ...", function(err) {
+			if (err) throw err;
+
+			console.log("Successfully inserted!");
+		});
+		
 
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.end(JSON.stringify(parameters));
 	}).listen(7000, "127.0.0.1");
-	console.log("Server running at http://127.0.0.1:7000");
+	console.log("Server is up and running at http://127.0.0.1:7000");
 }());
